1ce785ba37199b3eff299ab84216112c2f681545
Jothi Prasath <jothiprasath2@gmail.com>
2023-07-15 17:07:13 +0530

    optimized the code

--- setup.sh
+++ setup.sh
@@ -2,38 +2,34 @@
 
 # -----------------------------------------------------------------------------
 #   Author: Jothi Prasath
-#   Github: https://github.com/jothi-prasath/termux-dev-setup
+#   GitHub: https://github.com/jothi-prasath/termux-dev-setup
 # -----------------------------------------------------------------------------
 
-# Updating and upgrading packages
-pkg update
+# Update and upgrade packages
+pkg update -y
 pkg upgrade -y
 
-# Cleaning up packages
+# Clean up packages
 pkg autoclean
 pkg clean
 
-# Packages list
+# Packages list add or remove your needed packages
 packages_list=(
-  curl wget zip grep bc htop nano openssh rsync net-tools \
-  python python2 clang nodejs rust golang ruby \
-  sqlite perl openjdk-17 termux-exec termux-api \
-  )
+  curl wget zip grep bc htop nano openssh rsync net-tools
+  python python2 clang nodejs rust golang ruby
+  sqlite perl openjdk-17 termux-exec termux-api
+)
 
-# Converting array into a space-separated string
-packages="${packages_list[*]}"
+# Install the packages
+pkg install "${packages_list[@]}" -y
 
-# Installing the packages
-pkg install $packages -y
+# Setting up sshd daemon
+bashrc_path="$HOME/.bashrc"
+sshd_entry="sshd"
 
-# Setting up ssh deamon
-if [ -f ~/.bashrc ]; then
-    # Search for the sshd entry in .bashrc
-    if grep -q "sshd" ~/.bashrc; then
-        echo "sshd entry found in .bashrc"
-    else
-        echo "sshd" >> .bashrc
-    fi
+if ! grep -q "$sshd_entry" "$bashrc_path"; then
+    echo "$sshd_entry" >> "$bashrc_path"
+    echo "Added sshd entry to .bashrc"
 else
-    echo "sshd" >> .bashrc
+    echo "sshd entry already exists in .bashrc"
 fi
